8b70149b7914a2dc694c9801978fccd8ded6bc6a
ldonkers <ldonkers@gmail.com>
2022-12-23 22:04:55 +0000

    added several more files

---
+++ DoWhileLoop.java
@@ -0,0 +1,16 @@
+public class DoWhileLoop {
+    
+
+    public static void main(String[] args) {
+    
+        int counter=1;
+
+        do {
+            System.out.println("Times through this loop: " +counter);
+            counter++;
+        } while (counter <=10);
+
+    }
+
+
+}
---
+++ WhileLoop.java
@@ -0,0 +1,14 @@
+public class WhileLoop {
+    
+    public static void main(String[] args) {
+    
+        int counter=1;
+
+        while (counter <=10){
+            System.out.println("Times through this loop: " +counter);
+            counter++;
+        }
+
+    }
+
+}
